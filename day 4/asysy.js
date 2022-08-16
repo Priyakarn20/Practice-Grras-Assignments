af2a2c3c710bc1fb34f45b3e8088db4920017690
Priyakarn20 <priyakarn1997@gmail.com>
2022-08-16 23:38:42 +0530

    adding synchronous and asychronous functions

---
+++ day 4/asysy.js
@@ -0,0 +1,47 @@
+setTimeout(() => {
+    console.log('Enjoy each and every moment of this life ')
+}, 2000);
+
+setTimeout(() => {
+    console.log(" Hi Priya")
+},5000);
+
+console.log(" Be Happy Always ");
+
+setTimeout(() => {
+    console.log("Focus on your energy where it goes")
+}, 1000);
+
+console.log( " Be a good listener ");
+
+setTimeout(() => {
+console.log("Be conscious towards your life")
+},2500);
+
+console.log(" remember, you only got life once ");
+
+function life() {
+    let Happiness = 80;
+    let Sadness = 10;
+    let joy = 5;
+    console.log(Happiness);
+    console.log(joy);
+};
+life();
+
+function Courage() {
+    console.log("Be Bold");
+};
+setInterval(Courage, 2000);
+
+function love () {
+    console.log(" love everyone ");
+};
+setInterval(love, 1000);
+
+
+
+
+
+
+
